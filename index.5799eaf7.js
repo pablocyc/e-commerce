6c2d2842bec04dcce8ad835768a340204d3ac0ec
Pablo Cardozo <pablocyc83@gmail.com>
2023-01-17 18:27:50 -0400

    Updates

--- index.5799eaf7.js
+++ index.5799eaf7.js
@@ -1,4 +1,4 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))a(i);new MutationObserver(i=>{for(const s of i)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function e(i){const s={};return i.integrity&&(s.integrity=i.integrity),i.referrerpolicy&&(s.referrerPolicy=i.referrerpolicy),i.crossorigin==="use-credentials"?s.credentials="include":i.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(i){if(i.ep)return;i.ep=!0;const s=e(i);fetch(i.href,s)}})();const d=[{name:"French Fries with Ketchup",price:"4.50",image:"french-fries",color:"#7ab3f3"},{name:"Salmon and Vegetables",price:"5.12",image:"salmon-vegetables",color:"#e979b2"},{name:"Spaghetti with Sauce",price:"7.82",image:"spaghetti-meat-sauce",color:"#d7d7f9"},{name:"Bacon with Eggs",price:"2.23",image:"bacon-eggs",color:"#78f7bb"},{name:"Chicken and Salad",price:"5.03",image:"chicken-salad",color:"#7ab3f3"},{name:"Fish Sticks and Chips",price:"3.84",image:"fish-sticks-fries",color:"#e979b2"},{name:"Ravioli",price:"2.94",image:"ravioli",color:"#d7d7f9"},{name:"Tortellini",price:"4.99",image:"tortellini",color:"#78f7bb"}];class m extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"})}handleEvent(t){if(t.type==="click"){const e=t.composedPath()[0].dataset.action;let a=t.composedPath()[0],i=!0;if(e){e==="cart-text"&&(a=t.composedPath()[1]);let s=a.classList[1];const r=this.shadowRoot.querySelector(`.${s} img`),o=s.lastIndexOf("-");if(s=s.substring(o+1),r)i=!1,a.style.setProperty("--primary-color","#6b00f5"),a.innerText="Add to Cart";else{a.style.setProperty("--primary-color","#232323");const l=document.createElement("img");l.src="images/check.svg",l.alt="icon-check",a.innerText="In Cart",a.prepend(l)}const c=new CustomEvent("item-selected",{detail:{from:"Menu",index:s,status:i},bubbles:!0,composed:!0});this.dispatchEvent(c)}}}static get styles(){return`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const i of s)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function e(s){const i={};return s.integrity&&(i.integrity=s.integrity),s.referrerpolicy&&(i.referrerPolicy=s.referrerpolicy),s.crossorigin==="use-credentials"?i.credentials="include":s.crossorigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(s){if(s.ep)return;s.ep=!0;const i=e(s);fetch(s.href,i)}})();const m=[{name:"French Fries with Ketchup",price:"4.50",image:"french-fries",color:"#7ab3f3"},{name:"Salmon and Vegetables",price:"5.12",image:"salmon-vegetables",color:"#e979b2"},{name:"Spaghetti with Sauce",price:"7.82",image:"spaghetti-meat-sauce",color:"#d7d7f9"},{name:"Bacon with Eggs",price:"2.23",image:"bacon-eggs",color:"#78f7bb"},{name:"Chicken and Salad",price:"5.03",image:"chicken-salad",color:"#7ab3f3"},{name:"Fish Sticks and Chips",price:"3.84",image:"fish-sticks-fries",color:"#e979b2"},{name:"Ravioli",price:"2.94",image:"ravioli",color:"#d7d7f9"},{name:"Tortellini",price:"4.99",image:"tortellini",color:"#78f7bb"}];class d extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"})}handleEvent(t){if(t.type==="click"){const e=t.composedPath()[0].dataset.action;let a=t.composedPath()[0],s=!0;if(e){e==="cart-text"&&(a=t.composedPath()[1]);let i=a.classList[1];const o=this.shadowRoot.querySelector(`.${i} img`),r=i.lastIndexOf("-");if(i=i.substring(r+1),o)s=!1,a.style.setProperty("--primary-color","#6b00f5"),a.innerText="Add to Cart";else{a.style.setProperty("--primary-color","#232323");const l=document.createElement("img");l.src="images/check.svg",l.alt="icon-check",a.innerText="In Cart",a.prepend(l)}const c=new CustomEvent("item-selected",{detail:{from:"Menu",index:i,status:s},bubbles:!0,composed:!0});this.dispatchEvent(c)}}}static get styles(){return`
       :host {
       }
 
@@ -89,7 +89,7 @@
         width: 24px;
         width: 24px;
       }
-    `}unCheckButton(t){const e=this.shadowRoot.querySelector(`.index-${t}`);e.style.setProperty("--primary-color","#6b00f5"),e.innerText="Add to Cart"}setItems(){let t="";return d.forEach((e,a)=>{t+=`
+    `}unCheckButton(t){const e=this.shadowRoot.querySelector(`.index-${t}`);e.style.setProperty("--primary-color","#6b00f5"),e.innerText="Add to Cart"}setItems(){let t="";return m.forEach((e,a)=>{t+=`
       <div class="card" style="--card-color: ${e.color}">
         <img src="images/plate__${e.image}.png" alt="${e.plate}" class="plate-image">
         <main class="plate-contain">
@@ -100,13 +100,13 @@
           </button>
         </main>
       </div>`}),t}init(){this.button=this.shadowRoot.querySelector(".plates-items"),this.button.addEventListener("click",this)}connectedCallback(){this.render(),this.init()}render(){this.shadowRoot.innerHTML=`
-    <style>${m.styles}</style>
+    <style>${d.styles}</style>
     <div class="container">
       <h1 class="title">To Go Menu</h1>
       <div class="plates-items">
         ${this.setItems()}
       </div>
-    </div>`}}customElements.define("menu-items",m);class p extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"})}handleEvent(t){if(t.type==="click"){const e=t.composedPath()[1].dataset.button;if(e){const a=t.composedPath()[4].childNodes[1].lastElementChild,i=t.composedPath()[2].childNodes[3],s=t.composedPath()[2].childNodes[7];let r=parseInt(a.innerText);const o=e.split("-")[1];let c=this.getUnformat(s.innerText);if(e.split("-")[0]==="right"&&(r+=1,this.updatePrices(o),c+=this.getPrice(o)),e.split("-")[0]==="left"){if(r-=1,r<=0){this.deleteItem(o);const l=new CustomEvent("item-delete",{detail:{from:"Cart",index:o},bubbles:!0,composed:!0});this.dispatchEvent(l);return}this.updatePrices(o,!1),c-=this.getPrice(o)}s.innerText=this.getFormat(c),a.innerText=r.toString(),i.innerText=r.toString()}}}static get styles(){return`
+    </div>`}}customElements.define("menu-items",d);class p extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"})}handleEvent(t){if(t.type==="click"){const e=t.composedPath()[1].dataset.button;if(e){const a=t.composedPath()[4].childNodes[1].lastElementChild,s=t.composedPath()[2].childNodes[3],i=t.composedPath()[2].childNodes[7];let o=parseInt(a.innerText);const r=e.split("-")[1];let c=this.getUnformat(i.innerText);if(e.split("-")[0]==="right"&&(o+=1,this.updatePrices(r),c+=this.getPrice(r)),e.split("-")[0]==="left"){if(o-=1,o<=0){this.deleteItem(r);const l=new CustomEvent("item-delete",{detail:{from:"Cart",index:r},bubbles:!0,composed:!0});this.dispatchEvent(l);return}this.updatePrices(r,!1),c-=this.getPrice(r)}i.innerText=this.getFormat(c),a.innerText=o.toString(),s.innerText=o.toString()}}}static get styles(){return`
       :host {
       }
 
@@ -238,15 +238,15 @@
       .total-price {
         font-size: 2rem;
       }
-    `}setItem(t,e){e?(this.addItem(t),this.updatePrices(t)):this.deleteItem(t)}deleteItem(t){this.shadowRoot.querySelector(`.index-${t}`).remove(),this.updatePrices(t,!1)}addItem(t){const e=this.getFormat(d[t].price),a=document.createElement("template");a.innerHTML=`
+    `}setItem(t,e){e?(this.addItem(t),this.updatePrices(t)):this.deleteItem(t)}deleteItem(t){const e=this.shadowRoot.querySelector(`.index-${t}`),a=this.shadowRoot.querySelector(`.index-${t} .total-items`).innerText;e.remove(),this.updatePrices(t,!1,this.getUnformat(a))}addItem(t){const e=this.getFormat(m[t].price),a=document.createElement("template");a.innerHTML=`
       <div class="plate-shop index-${t}">
         <div class="main">
           <div class="image">
-            <img src="images/plate__${d[t].image}.png" alt="">
+            <img src="images/plate__${m[t].image}.png" alt="">
             <div class="quantity-image total-items">1</div>
           </div>
           <div class="description">
-            <h2 class="title-plate">${d[t].name}</h2>
+            <h2 class="title-plate">${m[t].name}</h2>
             <p class="price-unit">${e}</p>
             <div class="quantity">
               <button data-button="left-${t}">
@@ -262,7 +262,7 @@
         </div>
         <div class="line line-item"></div>
       </div>
-    `;const i=a.content.cloneNode(!0);this.cartItems.appendChild(i)}getPrice(t){return parseFloat(d[t].price)}updatePrices(t,e=!0){const a=this.getPrice(t);let i=this.getUnformat(this.prices[0].textContent);e?i+=a:i-=a;const s=i*.1,r=i+s;this.prices[0].textContent=this.getFormat(i),this.prices[1].textContent=this.getFormat(s),this.prices[2].textContent=this.getFormat(r)}getFormat(t){const e={style:"currency",currency:"USD"};return new Intl.NumberFormat("en-US",e).format(t)}getUnformat(t){return parseFloat(t.replace(/[^0-9.-]+/g,""))}init(){this.cartItems=this.shadowRoot.querySelector(".cart-items"),this.prices=this.shadowRoot.querySelectorAll(".price"),this.cartItems.addEventListener("click",this)}connectedCallback(){this.render(),this.init()}render(){this.shadowRoot.innerHTML=`
+    `;const s=a.content.cloneNode(!0);this.cartItems.appendChild(s)}getPrice(t){return parseFloat(m[t].price)}updatePrices(t,e=!0,a=1){const s=this.getPrice(t);let i=this.getUnformat(this.prices[0].textContent);e?i+=s:i-=s*a;const o=i*.1,r=i+o;this.prices[0].textContent=this.getFormat(i),this.prices[1].textContent=this.getFormat(o),this.prices[2].textContent=this.getFormat(r)}getFormat(t){const e={style:"currency",currency:"USD"};return new Intl.NumberFormat("en-US",e).format(t)}getUnformat(t){return parseFloat(t.replace(/[^0-9.-]+/g,""))}init(){this.cartItems=this.shadowRoot.querySelector(".cart-items"),this.prices=this.shadowRoot.querySelectorAll(".price"),this.cartItems.addEventListener("click",this)}connectedCallback(){this.render(),this.init()}render(){this.shadowRoot.innerHTML=`
     <style>${p.styles}</style>
     <div class="container">
       <h1 class="title">Your Cart</h1>
